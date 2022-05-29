3a4bb2116ccdf65a65b15218f73116f323a003a7
Пешко Андрей <apeshko@metaprime.ru>
2022-05-29 19:38:32 +0300

    remove applyFilter icon

--- components/ProductFilter.vue
+++ components/ProductFilter.vue
@@ -241,20 +241,6 @@ const filterHandler = () => {
 
     <span v-if="applyDisplay" class="flex justify-center items-center">
       <span class="mr-[5px]">Применить фильтр</span>
-      <svg
-        class="w-6 h-6"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M5 13l4 4L19 7"
-        ></path>
-      </svg>
     </span>
   </button>
 </template>
